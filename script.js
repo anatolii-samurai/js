549453d5eeebddff7fe963952d040e8581a252d6
anatolii-samurai <goddevil22@yandex.ru>
2023-11-01 19:33:31 +0400

    Старт обучения

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-const num = 20;
-
-if(num < 49){
-    console.log('Error');
-} else if (num > 100 ){
-    console.log('Много');
-} else {
-    console.log('ok');
-}
-
-console.log( NaN && 2 && undefined );
-
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
- 
-if (hamburger && cola || fries === 3 && nuggets) {
-   console.log('Done!')
-}else{
-    console.log('error');
-}
+let userName = "Ivan";
+let userAge = 13;
+let userHeight;
+console.log(userHeight);
