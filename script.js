c18975b1eecfc0123792714e407127b4568068db
anatolii-samurai <goddevil22@yandex.ru>
2023-11-19 19:33:44 +0400

    stopwatcher

--- script.js
+++ script.js
@@ -1,62 +1,212 @@
-// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
-// let rightAnswer = (year > 2015) ? console.log('Earlier'):
-// (year < 2015) ? console.log('Later'):
-// console.log('Right');
+// object
 
-
-
-// if (rightAnswer) {
-//     console.log( 'Вы правы!' )
-// }else if(year > 2015){
-//     console.log("Earlier");
-// }else if(year < 2015){
-//     console.log("Later");
-// }
-//  let workHour = prompt('Рабочее время нашего магазина','');
-
-//  if(workHour > 10 && workHour < 18){
-//     console.log('w');
-//  }else{
-//     console.log('c');
-//  };
-// function checkAge(age){
-//     if( age < 18){
-//         console.log('You are not allowed');
-//     }else if(age >= 18){
-//         console.log("You are welcome");
+// const phoneBook = {
+//     list:{
+//     'igor': 2345,
+//     'ivan': 1237,
+//     'elena':903456
+//     },
+//     add(name,number){
+//         this.list[name] = number;
+//     },
+//     delete(name){
+//         delete this.list[name]
 //     }
 // }
-// checkAge(18);
-// function add(a,b,operation){
-//     operation = sum;
-//     return sum = a + b;
+// // phoneBook.list.elena = 2222222;
+// // phoneBook.add('misha',56789);
+// // phoneBook.add('liza',3489);
+// // phoneBook.delete('ivan');
+// // console.log('ivan' in phoneBook.list);
+
+// // // console.log(phoneBook.list);
+// // for( const name in phoneBook.list){
+// //     console.log(`${name} - ${phoneBook.list[name]}`);
+// //     // console.log(phoneBook.list[name]);
+// // }
+// const phone_Book = Object.assign({},phoneBook)
+// // const phone_Book = {...phoneBook};
+// console.log(phone_Book);
+
+// let str = `Hello`;
+
+// // получаем первый символ
+// console.log( str[0] ); // H
+// console.log( str.at(3) ); // H
+
+// получаем последний символ
+// console.log( str[str.length - 1] ); // o
+
+// console.log( str.at(-3) ); // o
+
+// let strSec = 'Widget with id';
+
+// console.log( strSec.indexOf('W') ) // 12
+
+// let str = 'Ослик Иа-Иа посмотрел на виадук';
+
+// let target = 'Иа'; // цель поиска
+
+// let pos = 0;
+// while (true) {
+//   let foundPos = str.indexOf(target, pos);
+//   if (foundPos == -1) break;
+
+//   console.log( `Найдено тут: ${foundPos}` );
+//   pos = foundPos;
+//   ++pos; // продолжаем со следующей позиции
 // }
-// console.log(add(1,2,sum));
-// let add = '+';
-// function calc(a,b,operation){
-//     if(operation === 'add'){
-//         return a + b
-//     }else if (operation === 'multi'){
-//         return a * b
-//     }else if (operation ==='substract'){
-//         return a - b
+// let str = "stringify";
+// // 'strin', символы от 0 до 5 (не включая 5)
+// console.log( str.slice(0, 5) );
+// // 's', от 0 до 1, не включая 1, т. е. только один символ на позиции 0
+// console.log( str.slice(0,2) );
+
+// Напишите функцию которая преобразует полученную строку в "вертикальный вид" и выводит ее в консоль. 
+// function showVerticalMessage(str){
+//     if (str.length > 7){
+//         str = str.slice(0,6)
+//     }
+//     for(let char of str){
+//         if (char.startsWith('s')){
+//             // char="S"+ char.slice(1);
+//             char=char[0].toUpperCase() + char.slice(1);
+//         }      
+//         console.log(char);
 //     }
 // }
-// console.log(calc(1, 2, 'add'));
-// console.log(calc(1, 2, 'multi'));
-// console.log(calc(3, 2, 'substract'));
-let add = 0;
-function calc(a,b,operation){
-    switch(operation) {
-            case add:
-                return a + b;
-                case 'multi':
-                    return a * b;
-                        case 'substract':
-                            return a - b;
+// showVerticalMessage('stradastrada');
+// let toDoList = ['почитать', 'помыть посуду', 'помыть машину', 'посадить цветы'];
+// let firstTwoTasks = toDoList.splice(0, 1,'eat','drink');
+// console.log(firstTwoTasks);
+// console.log(toDoList);
+// let toDoList = ['почитать', 'помыть посуду', 'помыть машину', 'посадить цветы'];
+// let firstTask = toDoList.splice(2,2,'drink')
+// for(const task of toDoList){
+//     console.log(task);
+// }
 
-    } 
+const users = ['Ivan','Petr','Elena']
+users.forEach(user =>{
+    console.log(user);
+})
+
+// const numbers = [1,2,3,4,5,6,7,8,9];
+// numbers.forEach(number => {
+//     console.log(`Number is ${number}`);
+// })
+
+// const animals = ['lion','cat', 'dog', 'elephant', 'tiger', 'lion'];
+// const secondLionIndex = animals.find(animal => animal.length >= 5);
+// console.log(secondLionIndex); // 5
+
+// const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
+// const longWords = animals.filter(word => word.length < 5);
+// console.log(longWords); // ['elephant']
+
+// const numbers = [1, 11, -2, 3, -10, 4];
+// // const negativeNumbers = numbers.filter(number => number < 0);
+// // console.log(negativeNumbers);
+// const absoluteNumbers = numbers.map(number => Math.abs(number))
+// console.log(absoluteNumbers);
+
+// const numbers = [1, 11, -2, 3, -10, 4];
+// const sortedNumbers = numbers.sort((a, b) => b - a);
+// console.log(sortedNumbers); 
+
+// const string = 'cat,dog,elephant';
+// const animals = string.split(',');
+// console.log(animals); // ['cat', 'dog', 'elephant']
+// let count = 0;
+
+// function logCount() { // функция
+// 	let count = 1; // объявим локальную переменную count
+// 	console.log(count); // 0 или 1 ?
+// }
+
+// logCount(); // ???
+
+// let count = 0;
+
+// function logCount() {
+// 	console.log(count)
+// }
+
+// count = 40;
+// logCount(); // ???
+
+
+
+// function incrementCount() {
+//     let count = 0;
+//     return function (){
+//        return ++count;
+//     }
+// 	 // переменная count доступна из замыкания
+// }
+// let counterA = incrementCount();
+// let counterB = incrementCount();
+// console.log(counterA());
+// console.log(counterA());
+// console.log(counterB());
+// console.log(counterB());
+// console.log(counterB());
+
+// function sayMyName() {
+// 	console.log('Anatolii');
+// }
+
+// setTimeout(sayMyName, 5000);
+// sayMyName()
+
+// function hello() {
+// 	console.log('привет');
+// }
+// function goodbye() {
+// 	console.log('пока');
+// }
+
+// setTimeout(hello, 2000);
+// goodbye();
+
+// let timer;
+// let x = 11;
+// function countDown(){
+//     x--;
+//     if(x <= 0){
+//         clearTimeout(timer)
+//         console.log(`время вышло`);
+
+//     }else{
+//         timer = setTimeout(countDown,1000)
+//         console.log(`осталось секунд: ${x}`);
+        
+//     }
+   
+// }
+// countDown()
+// // 
+// let secondTimer;
+// let z = 11;
+// function countDownSecond(){
+//     z--;
+//     console.log(`осталось секунд: ${z}`);
+//      if( z <= 1) {
+//         clearInterval(secondTimer)
+//         console.log('Time is over');
+//      } 
+// }
+// secondTimer = setInterval(countDownSecond,1000)
+// countDownSecond()
+
+function buildFun(n){
+
+	var res = []
+
+	for (var i = 0; i< n; i++){
+		res.push((i => () => i)(i));
+	}
+
+	return res
 }
-console.log(calc(8, 2, add));
-console.log(calc(6, 2, 'multi'));
-console.log(calc(3, 2, 'substract'));
+buildFun(6)
